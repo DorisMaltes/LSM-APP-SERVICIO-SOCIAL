54de53a8753371eb4662b1beec412c27a410b0c1
Doris Elena Maltes <doris.maltescs@udlap.mx>
2025-01-12 06:40:25 -0600

    ultimo actualizado antes que le mete CoreML

---
+++ LSM-APP-SERVICIO-SOCIAL/QuizzView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  LSM-APP-SERVICIO-SOCIAL
+//
+//  Created by Doris Elena  on 08/01/25.
+//
+
+import SwiftUI
+
+struct QuizzView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizzView()
+}
