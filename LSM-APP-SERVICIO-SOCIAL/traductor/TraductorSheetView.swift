b878017c05106b76a85f2f0e3ba9305a9af991ca
Doris Elena Maltes <doris.maltescs@udlap.mx>
2025-01-12 07:43:08 -0600

    CoreML integrado en traductor, falta quizzes

--- LSM-APP-SERVICIO-SOCIAL/traductor/TraductorSheetView.swift
+++ LSM-APP-SERVICIO-SOCIAL/traductor/TraductorSheetView.swift
@@ -1,18 +1,18 @@
 //
-//  TraductorView.swift
+//  TraductorSheetView.swift
 //  LSM-APP-SERVICIO-SOCIAL
 //
-//  Created by Doris Elena  on 08/01/25.
+//  Created by Doris Elena  on 12/01/25.
 //
 
 import SwiftUI
 
-struct TraductorView: View {
+struct TraductorSheetView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TraductorView()
+    TraductorSheetView()
 }
