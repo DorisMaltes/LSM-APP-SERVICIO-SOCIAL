93e7beac3ea223d7c2ffa2e66c993c45968eceba
Doris Elena Maltes <doris.maltescs@udlap.mx>
2025-01-12 08:20:48 -0600

    producto acabadoooo yaaaay

---
+++ LSM-APP-SERVICIO-SOCIAL/Quizzes/ListaDeCategoriasQuizView.swift
@@ -0,0 +1,18 @@
+//
+//  ListaDeCategoriasQuizView.swift
+//  LSM-APP-SERVICIO-SOCIAL
+//
+//  Created by Doris Elena  on 12/01/25.
+//
+
+import SwiftUI
+
+struct ListaDeCategoriasQuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListaDeCategoriasQuizView()
+}
--- LSM-APP-SERVICIO-SOCIAL/Quizzes/QuizPreguntaSheetView.swift
+++ LSM-APP-SERVICIO-SOCIAL/Quizzes/QuizPreguntaSheetView.swift
@@ -1,18 +1,18 @@
 //
-//  QuizView.swift
+//  QuizPreguntaSheetView.swift
 //  LSM-APP-SERVICIO-SOCIAL
 //
-//  Created by Doris Elena  on 08/01/25.
+//  Created by Doris Elena  on 12/01/25.
 //
 
 import SwiftUI
 
-struct QuizzView: View {
+struct QuizPreguntaSheetView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    QuizzView()
+    QuizPreguntaSheetView()
 }
