be4b7c6fa850eb420187cb1e24b9ed953838d909
Daniel Martinez Maimone <96289800+ZapatoProgramming@users.noreply.github.com>
2025-01-21 15:04:48 -0600

    :0

--- LSM-APP-SERVICIO-SOCIAL/traductor/ResumenFraseSheetView.swift
+++ LSM-APP-SERVICIO-SOCIAL/traductor/ResumenFraseSheetView.swift
@@ -14,7 +14,7 @@ struct ResumenFraseSheetView: View {
 
     var body: some View {
         VStack(spacing: 20) {
-            Text("Frase final:")
+            Text("Frase final: xddd")
                 .font(.headline)
 
             Text(frase.joined(separator: " "))
